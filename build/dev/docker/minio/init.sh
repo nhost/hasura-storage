899a1bf9e3d4e37c8a3ad054ce3430871dd739ca
David Barroso <dbarrosop@dravetech.com>
2022-01-07 15:09:07 +0100

    implemented DELETE file/:id

--- build/dev/docker/minio/init.sh
+++ build/dev/docker/minio/init.sh
@@ -8,6 +8,6 @@ mc alias set myminio http://localhost:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 mc mb myminio/default
 
 dd if=/dev/random of=/tmp/asd bs=64k count=1
-mc cp /tmp/asd myminio/default/default
+mc cp /tmp/asd myminio/default/default/asd
 
 sleep infinity
