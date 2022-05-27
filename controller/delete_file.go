29aec1f45c97ee96a44d8dc567dcb02a62eafba4
David Barroso <dbarrosop@dravetech.com>
2022-05-27 16:17:58 +0200

    fix: dont require select:bucket permissions to delete files

--- controller/delete_file.go
+++ controller/delete_file.go
@@ -7,30 +7,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// this type is used to ensure we respond consistently no matter the case.
-type DeleteFileResponse struct {
-	ProcessedFile FileMetadata `json:"processedFile,omitempty"`
-}
-
-func (ctrl *Controller) deleteFile(ctx *gin.Context) (FileMetadata, *APIError) {
+func (ctrl *Controller) deleteFile(ctx *gin.Context) *APIError {
 	id := ctx.Param("id")
 
-	fileMetadata, apiErr := ctrl.metadataStorage.DeleteFileByID(ctx.Request.Context(), id, ctx.Request.Header)
+	apiErr := ctrl.metadataStorage.DeleteFileByID(ctx.Request.Context(), id, ctx.Request.Header)
 	if apiErr != nil {
-		return FileMetadata{}, apiErr
+		return apiErr
 	}
 
-	if apiErr := ctrl.contentStorage.DeleteFile(fileMetadata.ID); apiErr != nil {
-		return FileMetadata{}, apiErr
+	if apiErr := ctrl.contentStorage.DeleteFile(id); apiErr != nil {
+		return apiErr
 	}
 
 	ctx.Set("FileChanged", id)
 
-	return fileMetadata.FileMetadata, nil
+	return nil
 }
 
 func (ctrl *Controller) DeleteFile(ctx *gin.Context) {
-	fileMetadata, apiErr := ctrl.deleteFile(ctx)
+	apiErr := ctrl.deleteFile(ctx)
 	if apiErr != nil {
 		_ = ctx.Error(fmt.Errorf("problem processing request: %w", apiErr))
 
@@ -39,5 +34,5 @@ func (ctrl *Controller) DeleteFile(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusNoContent, DeleteFileResponse{fileMetadata})
+	ctx.AbortWithStatus(http.StatusNoContent)
 }
